a513967de53cf7fbbe81902173adb60cc24bba78
Víctor Pérez Masegosa <vipermaseg@gmail.com>
2024-02-01 03:15:14 +0100

    Adding dbcontext

---
+++ api/UserAdminApi/UserAdminApi/DbModel/UserAdminDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Reflection.Metadata;
+
+namespace UserAdminApi.DbModel;
+public class UserAdminDbContext : DbContext
+{
+    public UserAdminDbContext(DbContextOptions<UserAdminDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public Int64 Credits { get; set; }
+}
